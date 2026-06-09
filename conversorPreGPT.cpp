b9b090491eae673020f7f979f93a4c86c9e31ecd
mayan <161239309+maayan0@users.noreply.github.com>
2026-06-09 17:21:40 -0300

    Add battery voltage monitoring functionality

---
+++ conversorPreGPT.cpp
@@ -0,0 +1,121 @@
+//VALOR 12BITS=>AR=>valorAnalog
+
+//VALOR REAL BATERIA => valorRealBateria
+
+#include <LiquidCrystal.h>
+
+//const int pinADC = 36;
+
+
+
+float valorRealBateria = 0;
+
+float valorReferencia = 8.4;
+
+void setup(){
+
+  Serial.begin(9600);
+
+  //pinMode(pinADC, INPUT);
+
+}
+
+void loop(){
+
+//faz a leitura do valor de 12 bits analogico obtido pelo pino adc
+
+//float valorLidoADC = analogRead(pinADC);
+
+
+int valorLidoADC = 4095;
+
+valorRealBateria = (valorLidoADC / 487.5);
+
+float porcentagemBateria = ((valorRealBateria/valorReferencia)*100);
+
+valorRealBateria -= 0.1;
+
+//PRINT VALOR DA TENSAO DA BATERIA:
+
+Serial.print("Tensão da Bateria: ");
+
+Serial.print(valorRealBateria);
+
+Serial.print("V ");
+
+  //CASO 1 => BATERIA 8,4V - 8V --> 100%
+
+  if (8.4>=valorRealBateria && valorRealBateria>8){
+
+    Serial.print("Bateria: ");
+
+    Serial.print(porcentagemBateria);
+
+    Serial.println("%");
+
+    Serial.println("STATUS: OK - BATERIA SEGURA");
+
+  }
+
+  //CASO 2 => BATERIA 8 - 7,4V --> 80%
+
+  else if (8>=valorRealBateria && valorRealBateria>7.4){
+
+    Serial.print("Bateria: ");
+
+    Serial.print(porcentagemBateria);
+
+    Serial.println("%");
+
+    Serial.println("STATUS: OK - BATERIA SEGURA");    
+
+  }
+
+  //CASO 3 => BATERIA 7,4V - 6,8V --> 60%
+
+  else if (7.4>=valorRealBateria && valorRealBateria > 6.8){
+
+    Serial.print("Bateria: ");
+
+    Serial.print(porcentagemBateria);
+
+    Serial.println("%");
+
+    Serial.println("STATUS: ALERTA - BATERIA BAIXA");
+
+  }
+
+  //CASO 4 => BATERIA 6,8V - 6,4V --> 20%
+
+  else if (6.8>=valorRealBateria && valorRealBateria > 6.4){
+
+    Serial.print("Bateria: ");
+
+    Serial.print(porcentagemBateria);
+
+    Serial.println("%");
+
+    Serial.println("STATUS: ALERTA - RECARREGAR BATERIA");    
+
+  }
+
+  //CASO 5 => BATERIA ABAIXO DE 6,4V --> 0%
+
+    else if (valorRealBateria < 6.4){
+
+    Serial.print("Bateria: ");
+
+    Serial.print(porcentagemBateria);
+
+    Serial.println("%");
+
+    Serial.println("STATUS: CRÍTICO - BATERIA DANIFICADA");    
+
+  }
+
+ 
+
+  delay(5000);
+
+
+} 
