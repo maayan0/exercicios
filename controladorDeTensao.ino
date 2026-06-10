80d3e2e698abd71faf41e51a45d0ccc6819f3ddb
mayan <161239309+maayan0@users.noreply.github.com>
2026-06-10 20:23:14 -0300

    Refactor battery voltage display to use LCD

--- controladorDeTensao.ino
+++ controladorDeTensao.ino
@@ -1,78 +1,94 @@
-// VALOR 12BITS=>AR=>valorAnalog
-// VALOR REAL BATERIA => valorRealBateria
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-#include <LiquidCrystal.h>
+#define col 16
+#define lin 2
+#define ende 0x3F
+LiquidCrystal_I2C lcd(ende,16,2);
 
-// const int pinADC = 36;
-
-float valorRealBateria = 8.4;      // Valor inicial da simulação
+bool v = LOW;
+int valorBinario = 4095;
+float valorRealBateria = (valorBinario/487.5);      // Valor inicial da simulação
 float valorReferencia = 8.4;
 float porcentagemBateria;
 
 void setup() {
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
   // pinMode(pinADC, INPUT);
 }
 
 void loop() {
-
+  lcd.setCursor(1,0);
   // Recalcula a porcentagem a cada leitura
   porcentagemBateria = (valorRealBateria / valorReferencia) * 100.0;
 
   // PRINT VALOR DA TENSAO DA BATERIA
-  Serial.print("Tensao da Bateria: ");
-  Serial.print(valorRealBateria, 1); // Exibe 1 casa decimal
-  Serial.print("V ");
+  lcd.setCursor(0,0);
+  lcd.print("Tensao da Bateria: ");
+  lcd.print(valorRealBateria, 1); // Exibe 1 casa decimal
+  lcd.print("V ");
 
   // CASO 1 => BATERIA 8,4V - 8V --> 100%
   if (8.4 >= valorRealBateria && valorRealBateria > 8.0) {
-    Serial.print("Bateria: ");
-    Serial.print(porcentagemBateria, 1);
-    Serial.println("%");
-    Serial.println("STATUS: OK - BATERIA SEGURA");
+    lcd.setCursor(0,1);
+    lcd.print("Bateria: ");
+    lcd.print(porcentagemBateria, 1);
+    lcd.println("%");
+    lcd.println("STATUS: OK - BATERIA SEGURA");
   }
 
   // CASO 2 => BATERIA 8V - 7,4V --> 80%
   else if (8.0 >= valorRealBateria && valorRealBateria > 7.4) {
-    Serial.print("Bateria: ");
-    Serial.print(porcentagemBateria, 1);
-    Serial.println("%");
-    Serial.println("STATUS: OK - BATERIA SEGURA");
+    lcd.setCursor(0,1);
+    lcd.print("Bateria: ");
+    lcd.print(porcentagemBateria, 1);
+    lcd.println("%");
+    lcd.println("STATUS: OK - BATERIA SEGURA");
   }
 
   // CASO 3 => BATERIA 7,4V - 6,8V --> 60%
   else if (7.4 >= valorRealBateria && valorRealBateria > 6.8) {
-    Serial.print("Bateria: ");
-    Serial.print(porcentagemBateria, 1);
-    Serial.println("%");
-    Serial.println("STATUS: ALERTA - BATERIA BAIXA");
+    lcd.setCursor(0,1);
+    lcd.print("Bateria: ");
+    lcd.print(porcentagemBateria, 1);
+    lcd.println("%");
+    lcd.println("STATUS: ALERTA - BATERIA BAIXA");
   }
 
   // CASO 4 => BATERIA 6,8V - 6,4V --> 20%
   else if (6.8 >= valorRealBateria && valorRealBateria > 6.4) {
-    Serial.print("Bateria: ");
-    Serial.print(porcentagemBateria, 1);
-    Serial.println("%");
-    Serial.println("STATUS: ALERTA - RECARREGAR BATERIA");
+    lcd.setCursor(0,1);
+    lcd.print("Bateria: ");
+    lcd.print(porcentagemBateria, 1);
+    lcd.println("%");
+    lcd.println("STATUS: ALERTA - RECARREGAR BATERIA");
   }
 
   // CASO 5 => BATERIA ABAIXO DE 6,4V --> 0%
   else if (valorRealBateria <= 6.4) {
-    Serial.print("Bateria: ");
-    Serial.print(porcentagemBateria, 1);
-    Serial.println("%");
-    Serial.println("STATUS: CRITICO - BATERIA DANIFICADA");
+    lcd.setCursor(0,1);
+    lcd.print("Bateria: ");
+    lcd.print(porcentagemBateria, 1);
+    lcd.println("%");
+    lcd.println("STATUS: CRITICO - BATERIA DANIFICADA");
   }
 
-  Serial.println();
+  lcd.println();
 
   // Simulação: reduz 0,1 V por ciclo
-  valorRealBateria =- 0.1;
+  valorRealBateria -= 0.1;
 
   // Evita tensões negativas
-  if (valorRealBateria < 0.0) {
+  if (valorRealBateria <= 0.0) {
     valorRealBateria = 0.0;
+    v = HIGH;
+    if(v == HIGH){ lcd.print("Bateria: "); lcd.print(valorRealBateria); lcd.println("%"); delay(1000); exit(0);}
   }
 
-  delay(900);
+  delay(3000);
+
+  lcd.clear();
 }
